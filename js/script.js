23a2473fd48464e04a1679913ed9bd2ece1f2570
dzarrorn <zarrorn@gmail.com>
2021-11-26 01:33:20 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+let navbar = document.querySelector(".navbar");
+document.querySelector('#burger').onclick = () => {
+    navbar.classList.toggle('active');
+}
+window.onscroll = () => {
+    navbar.classList.remove('active');
+}
